660cacbcea8974686f2aaf4f98f16cd074276b54
Jean-Baptiste Claramonte <jbclaramonte@xebia.fr>
2014-03-22 12:17:25 +0100

    Step 1 : blank project

---
+++ todo-api/src/main/java/fr/xebia/gae/todo/api/v1/TodoEndPoint.java
@@ -0,0 +1,11 @@
+package fr.xebia.gae.todo.api.v1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbclaramonte
+ * Date: 3/22/14
+ * Time: 12:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TodoEndPoint {
+}
